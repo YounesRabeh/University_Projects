09f124e309a55fa99955e4240827976e6683c132
Younes Rabeh <93033194+YounesRabeh@users.noreply.github.com>
2023-04-26 00:05:48 +0200

    Delete main.py

--- my_manager/app/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-import os
-# Add the parent directory of the 'app' folder to the Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from app.graphics.gui import *
-
-app = TkinterApp()
-app.mainloop()
