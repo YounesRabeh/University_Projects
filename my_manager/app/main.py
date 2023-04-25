586b41994e33edcd5357050d000c40689feaca23
Younes Rabeh <93033194+YounesRabeh@users.noreply.github.com>
2023-04-25 18:23:40 +0200

    Update main.py

--- my_manager/app/main.py
+++ my_manager/app/main.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+import sys
+import os
+# Add the parent directory of the 'app' folder to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app.graphics.gui import *
 
 app = TkinterApp()
