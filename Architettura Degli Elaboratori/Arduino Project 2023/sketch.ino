8c6aa1de464363991aca5d3fd075bf370ba55b6a
Younes Rabeh <93033194+YounesRabeh@users.noreply.github.com>
2024-03-08 17:41:54 +0100

    update

--- Architettura Degli Elaboratori/Arduino Project 2023/sketch.ino
+++ Architettura Degli Elaboratori/Arduino Project 2023/sketch.ino
@@ -1,169 +1,223 @@
-//DECODE VARIABLES:
-volatile byte pulseIndex = 0;
-volatile uint8_t signalReceived = 0B00000000;
-uint64_t storedSignals = 0; //STORED INFO
 
-//TIMER VARIABLES:
-volatile int16_t delaTimeInerrupt;
-volatile int16_t previousDeltaTime;
-volatile int16_t storedTime;
 
-//PROTOCOL DETECTION VARIABLES:
-byte protocolType;
-short protocolStartBurstLength[] = {2220, 1110, 840, 590};
-//0: NEC, 1:SAMSUNG, 2:JAPAN, 3:SIRCS
-bool midwayFlag = false;
 
-void setup(){
-  Serial.begin(9600);
-  outputSetUp();
-  timerSetUp();
-}
-void loop(){
+//________________________________________________________________________________________________________________________________________
+// ENVIRONMENT:
+#define NO_PROTOCOL_DETECTED 255
+#define ERROR_MARGIN 64 // [4 interrupt overlap](16*4) = 64µs
+#define ERROR_BIT 254
 
-}
 
-void outputSetUp(){
-  DDRD   &= ~(1 << DDD2);    //Pin 2 as input
-  PORTD  |= (1 << PORTD2);   //Enable internal pull-up resistor on pin 2
-  PCICR  |= (1 << PCIE2);    // Enable Pin Change Interrupt 2
-  PCMSK2 |= (1 << PCINT18);  // Enable interrupt for Pin 2
-}
+volatile uint8_t  DETECTED_PROTOCOL = NO_PROTOCOL_DETECTED;
+volatile uint64_t _SAVED_DATA_;
 
-void timerSetUp(){
-  TCCR1A  = 0; 
-  TCCR1B  = 0;
-  TCNT1   = 0; 
-  TCCR1B |= (1 << CS11) | (1 << CS10); //PRESCALLAR 64:
-  OCR1A   = 16875;                     // (67,500 / 4) (16875) old
-  TIMSK1 |= (1 << OCIE1A);             // Enable timerinterrupt
-  TCCR1B |= (1 << WGM12);
-}
+#define STF_LEADING_PULSE_FACTOR    0
+#define STF_TAILING_PULSE_FACTOR    1
+#define DATA_LENGTH                 2
+#define DATA_STARTING_PULSE         3
+#define D0_LEADING_PULSE_STATE      4
+#define D0_LEADING_PULSE_FACTOR     5
+#define D0_TAILING_PULSE_FACTOR     6
+#define D1_LEADING_PULSE_FACTOR     7
+#define D1_TAILING_PULSE_FACTOR     8
+#define PROTOCOL_FULL_LENGTH        9
+#define END_LEADING_PULSE_FACTOR    10
+#define END_TAILING_PULSE_FACTOR    11
+#define REPEATE_INTERVAL_FACTOR     12
 
-ISR(TIMER1_COMPA_vect) {     //Every 67,500 ms
-  PCMSK2 |= (1 << PCINT18);  // Enable interrupt for Pin 2 
-  pulseIndex = 0;
-  midwayFlag = false;
-}
+//________________________________________________________________________________________________________________________________________
+// PROTOCOLS:
+#define NUM_SUPPORTED_PROTOCOLS 5
 
-ISR(PCINT2_vect) {          //PIN2 interrupt
-  if (pulseIndex == 0){timerSetUp();}
-  delaTimeInerrupt = TCNT1 - storedTime;
-  storedTime = TCNT1;
+#define NEC_PULSE_TIME     560
+#define SAMSUNG_PULSE_TIME 560
+#define JAPAN_PULSE_TIME   420
+#define SIRCS_PULSE_TIME   600
+#define RC5_PULSE_TIME     889
 
-  if (pulseIndex - 2 <= 0){
-    StartOfFrameCheck(delaTimeInerrupt); pulseIndex++; return;
-  }
-  if ((pulseIndex > 34 && pulseIndex < 51)){
-    if (pulseIndex % 2 == 1){previousDeltaTime = delaTimeInerrupt;}
-    else {
-      if((previousDeltaTime - delaTimeInerrupt > -2)){decode(0);}
-      else {decode(1);}
+const uint8_t  NEC_Data[]      = {16, 8,  8, 35,  0, 1, 1, 1, 3,  68, 1, 1, 193};
+const uint8_t  SAMSUNG_Data[]  = { 8, 8,  8, 35,  0, 1, 1, 1, 3,  68, 1, 1, 107};
+const uint8_t  JAPAN_Data[]    = { 8, 4,  8, 67,  0, 1, 1, 1, 4,  98, 0, 0, 100};
+const uint8_t  SIRCS_Data[]    = { 4, 1,  7,  3,  0, 1, 1, 2, 1,  26, 0, 0,  75};
+const uint8_t  RC5_Data[]      = { 1, 1,  6, 17,  1, 1, 1, 1, 1,  28, 0, 0, 128};
+
+const uint16_t SUPPORTED_PROTOCOLS_PULSE_TIME[NUM_SUPPORTED_PROTOCOLS] = {
+  NEC_PULSE_TIME,
+  SAMSUNG_PULSE_TIME,
+  JAPAN_PULSE_TIME,
+  SIRCS_PULSE_TIME,
+  RC5_PULSE_TIME,
+};
+
+const uint8_t* SUPPORTED_PROTOCOLS_DATA[NUM_SUPPORTED_PROTOCOLS] = {
+  NEC_Data,
+  SAMSUNG_Data,
+  JAPAN_Data,
+  SIRCS_Data,
+  RC5_Data,
+};
+
+// SET AFTER A PROTOCOL IS SELECTED:
+volatile uint16_t D0_LeadingPulse_Duration;
+volatile uint16_t D0_TailingPulse_Duration;
+volatile uint16_t D1_LeadingPulse_Duration;
+volatile uint16_t D1_TailingPulse_Duration;
+volatile uint8_t  D0_LeadingPulse_State;
+volatile uint8_t  DataStartingPulse;
+volatile uint8_t  DataEndingPulse;
+volatile uint8_t  LastPulse;
+volatile int32_t RepeatedSignalInterval; //To stop at start (no protocol detected yet)
+
+void protocolDetection(uint16_t STF_FPD, uint16_t STF_SPD){
+  for (uint8_t i = 0; i < NUM_SUPPORTED_PROTOCOLS; i++){
+    volatile uint16_t protocol_PulseTime  = SUPPORTED_PROTOCOLS_PULSE_TIME[i];
+    volatile const uint8_t* protocol_Data  = SUPPORTED_PROTOCOLS_DATA[i];
+    volatile int16_t pusleDuration = (protocol_PulseTime * protocol_Data[STF_LEADING_PULSE_FACTOR]) - (STF_FPD * 16);
+
+    if (abs(pusleDuration) <= ERROR_MARGIN){
+      pusleDuration = (protocol_PulseTime * protocol_Data[STF_TAILING_PULSE_FACTOR]) - (STF_SPD * 16);
+      if (abs(pusleDuration) <= ERROR_MARGIN){
+        DETECTED_PROTOCOL = i;
+        protocolEnvironmentVariablesSetup(protocol_PulseTime, protocol_Data);
+        return;
+      }
     }
   }
-  pulseIndex++;
-  if(pulseIndex == 52){
-    storeData(); 
-    buttons();
-    //data collected
-  }
 }
 
-void decode(byte data) {
-  signalReceived >>= 1;           // Shift everything to the left
-  signalReceived |= (data << 7);  // Add the new bit
-}  
-
-void storeData(){
-  storedSignals = (storedSignals << 8) | signalReceived;
+void protocolEnvironmentVariablesSetup(volatile const uint16_t DetectedProtocol_PulseTime, volatile const uint8_t* protocol_Data){
+  D0_LeadingPulse_Duration = protocol_Data[D0_LEADING_PULSE_FACTOR] * DetectedProtocol_PulseTime;
+  D0_TailingPulse_Duration = protocol_Data[D0_TAILING_PULSE_FACTOR] * DetectedProtocol_PulseTime;
+  D1_LeadingPulse_Duration = protocol_Data[D1_LEADING_PULSE_FACTOR] * DetectedProtocol_PulseTime;
+  D1_TailingPulse_Duration = protocol_Data[D1_TAILING_PULSE_FACTOR] * DetectedProtocol_PulseTime;
+  D0_LeadingPulse_State = protocol_Data[D0_LEADING_PULSE_STATE];
+  DataStartingPulse = protocol_Data[DATA_STARTING_PULSE];
+  DataEndingPulse = DataStartingPulse + (protocol_Data[DATA_LENGTH] * 2);
+  LastPulse = protocol_Data[PROTOCOL_FULL_LENGTH];
+  RepeatedSignalInterval = ((int32_t) protocol_Data[REPEATE_INTERVAL_FACTOR]) * (int32_t) DetectedProtocol_PulseTime;
 }
 
-void StartOfFrameCheck(int16_t time){
-  if (pulseIndex == 0){return;}
-  if (pulseIndex == 1){
-    for (byte i = 0; i < 5; i++){
-      if(time - protocolStartBurstLength[i] >= 0){
-        midwayFlag = true; protocolType = i;
+//________________________________________________________________________________________________________________________________________
+// SIGNAL:
+volatile int32_t timeFromSTF;
+volatile uint16_t pulse = 0;
+volatile uint16_t firstPulseDuration = 0;
+volatile bool isRepeatedSignal = false;
+volatile byte _DATA_;
+
+ISR(PCINT2_vect) {
+  volatile const uint16_t CURRENT_TIME = TCNT1; timeFromSTF += CURRENT_TIME; TCNT1 = 0; pulse++;
+  if (DETECTED_PROTOCOL == NO_PROTOCOL_DETECTED){
+    if (pulse == 1) {repeatedSignalCheck(); return;}
+    if (pulse == 2) {firstPulseDuration = CURRENT_TIME;}
+    if (pulse == 3) {protocolDetection(firstPulseDuration, CURRENT_TIME); _DATA_ = 0;}
+    return;
+  } else {
+    if (pulse >= DataStartingPulse && pulse <= DataEndingPulse) { // 35 (low first interr data)
+    if (pulse % 2 == 0) {
+      firstPulseDuration = CURRENT_TIME;
+    } else {
+        if (pulse != DataStartingPulse) { // firstTime no action
+          determineDataBit(firstPulseDuration, CURRENT_TIME) ? decode_DATA(1) : decode_DATA(0);
+        } else {
+          firstPulseDuration = 0; // Reset firstPulseDuration when pulse equals 35
+          //first interrupt of data
+        }
         return;
       }
     }
   }
 
-  if (pulseIndex == 2 && midwayFlag == true){
-    switch (protocolType){
-      case 0: if(time - protocolStartBurstLength[1] >= 0){return;} break;
-      case 1: if(time - protocolStartBurstLength[1] >= 0){return;} break;
-      case 2: if(time - protocolStartBurstLength[2] / 2 >= 0){return;} break;
-      case 3: if(time - protocolStartBurstLength[3] / 4 >= 0){return;} break;
+  if (pulse == LastPulse) {
+    //DONE//
+    pulse = 0;
+    save_DATA();
+    DETECTED_PROTOCOL = NO_PROTOCOL_DETECTED;
+  }
+}
+
+bool repeatedSignalCheck(){
+  if (_DATA_ == 0){return true;}
+  volatile int32_t signalDuration = RepeatedSignalInterval - timeFromSTF;
+  timeFromSTF = 0;
+  if (signalDuration > ERROR_MARGIN && abs(signalDuration) <= (ERROR_MARGIN * 50)){ //3200µs
+    timeout(abs(signalDuration));
+    save_DATA();
+    return true;
+  }
+  return false;
+}
+
+//________________________________________________________________________________________________________________________________________
+// FUNCTIONALITIES:
+
+void decode_DATA(byte bitValue) {
+  _DATA_ = (_DATA_ >> 1) | ((bitValue) << 7);
+}
+
+static void save_DATA() {
+  _SAVED_DATA_ <<= 8; // Shift _SAVED_DATA_ by 8 bits to make room for the new byte
+  _SAVED_DATA_ |= _DATA_; // OR the new data with _SAVED_DATA_
+}
+
+uint8_t determineDataBit(uint16_t DX_FPD, uint16_t DX_SPD){
+  volatile int16_t pulseDuration = D0_LeadingPulse_Duration - (DX_FPD * 16); //from -32,768 to 32,767 inclusively. firstPulse
+
+  if ((pinState() == D0_LeadingPulse_State) && abs(pulseDuration) <= ERROR_MARGIN){
+    pulseDuration = D0_TailingPulse_Duration - (DX_SPD * 16); //secondPulse
+    if (abs(pulseDuration) <= ERROR_MARGIN){
+      return 0;
     }
   }
-  PCMSK2 &= ~(1 << PCINT18);// Disable interrupt for Pin 2
-}
-void buttons(){
-  Serial.print("You pressed: ");
-  switch (signalReceived) {
-    case 162:
-      Serial.println("POWER");
-      break;
-    case 226:
-      Serial.println("MENU");
-      break;
-    case 34:
-      Serial.println("TEST");
-      break;
-    case 2:
-      Serial.println("PLUS");
-      break;
-    case 194:
-      Serial.println("BACK");
-      break;
-    case 224:
-      Serial.println("PREV.");
-      break;
-    case 168:
-      Serial.println("PLAY");
-      break;
-    case 144:
-      Serial.println("NEXT");
-      break;
-    case 104:
-      Serial.println("0");
-      break;
-    case 152:
-      Serial.println("MINUS");
-      break;
-    case 176:
-      Serial.println("C");
-      break;
-    case 48:
-      Serial.println("1");
-      break;
-    case 24:
-      Serial.println("2");
-      break;
-    case 122:
-      Serial.println("3");
-      break;
-    case 16:
-      Serial.println("4");
-      break;
-    case 56:
-      Serial.println("5");
-      break;
-    case 90:
-      Serial.println("6");
-      break;
-    case 66:
-      Serial.println("7");
-      break;
-    case 74:
-      Serial.println("8");
-      break;
-    case 82:
-      Serial.println("9");
-      break;
-    default:
-      Serial.print("UNKNOWN::SignalCode[byte]: ");
-      Serial.println(signalReceived);     
+  pulseDuration = D1_LeadingPulse_Duration - (DX_FPD * 16);   //firstPulse
+  if (abs(pulseDuration) <= ERROR_MARGIN){
+    pulseDuration = D1_TailingPulse_Duration - (DX_SPD * 16); //secondPulse
+    if (abs(pulseDuration) <= ERROR_MARGIN){
+      return 1;
+    }
   }
+  return ERROR_BIT; //ERROR
+}
+
+bool pinState(){return (PIND & (1 << PD2)) != 0;}
+
+//________________________________________________________________________________________________________________________________________
+// SETUP:
+void timerSetup() {
+  TCCR1A = 0;  // Clear Timer/Counter 1 Control Register A
+  TCCR1B = 0;  // Clear Timer/Counter 1 Control Register B
+  TCNT1 = 0;   // Clear Timer/Counter 1
+  TCCR1B |= (1 << CS12);  // PresDurationer 256 (1 -> 16µs)
+  OCR1A = 65535;  // Set the compare match value (1,04856s) MAX
+  TIMSK1 |= (1 << OCIE1A); // Enable timer interrupt on compare match
+  TCCR1A |= (1 << WGM12); // Set the Waveform Generation mode (WGM) to CTC mode
+}
+
+ISR(TIMER1_COMPA_vect) {
+  PCMSK2 |= (1 << PCINT18);
+  OCR1A = 65535;
+}
+
+void timeout(int32_t timeLeft){
+  PCMSK2 &= ~(1 << PCINT18);
+  TCNT1 = 0;
+  OCR1A = timeLeft / 16; //all timeout
+  _DATA_ = 0;
+}
+
+void outputSetup(){
+  DDRD &= ~(1 << DDD2);     // Set Pin 2 as input (clear bit 2 in DDRD)
+  PORTD |= (1 << PORTD2);   // Enable internal pull-up resistor on Pin 2 (set bit 2 in PORTD)
+  PCICR |= (1 << PCIE2);    // Enable Pin Change Interrupt for PCINT23..16 (PCICR bit 2)
+  PCMSK2 |= (1 << PCINT18); // Enable interrupt for Pin Change Enable Mask Register 2, bit 2 (PCINT18)
+}
+
+//________________________________________________________________________________________________________________________________________
+//DRIVER CODE:
+int main(void) {
+  sei();
+  timerSetup();
+  outputSetup();
+  while(1) {}
+  return 0;
 }
